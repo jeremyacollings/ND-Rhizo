b6cf33d693b348fbd82448841f5b1de94fb55d61
jeremyacollings <76605152+jeremyacollings@users.noreply.github.com>
2024-11-03 11:30:05 -0800

    feat: consolidated model fitting and added streamlined germ and mort rate models

--- 2_Fitting.Models.R
+++ 2_Fitting.Models.R
@@ -28,28 +28,31 @@ stan_dat <- list(N = nrow(fec_dat),
                  AA = fec_dat$B_comp, CG = fec_dat$C_comp, LB = fec_dat$A_comp, 
                  fec = round(fec_dat$fit))
 
-# Fitting candidate models ------------------------------------------------
+stan_dat2 <- list(N = nrow(germ_dat2), S = n_distinct(germ_dat2$sp),
+                  germ = germ_dat2$germ, 
+                  sp = ifelse(germ_dat2$sp == "B", 1, 
+                              ifelse(germ_dat2$sp == "C", 2, 3)))
 
-LotB <- stan(file = file.path("Stan_Mods", "LotB.stan"), 
-             data = stan_dat, chains = 2, cores = detectCores())
+stan_dat3 <- list(N = nrow(fec_dat), S = n_distinct(fec_dat$sp), 
+                  mort = ifelse(fec_dat$fit == 0, 1, 0), 
+                  sp = ifelse(fec_dat$sp == "B", 1, 
+                              ifelse(fec_dat$sp == "C", 2, 3)), 
+                  micro = ifelse(fec_dat$rhizobia == "sterile", 1, 2), 
+                  cont = ifelse(fec_dat$nitrogen == "zero nitrogen", 1, 2))
 
-LotUB <- stan(file = file.path("Stan_Mods", "LotUB.stan"), 
-             data = stan_dat, chains = 2, cores = detectCores())
+# Fitting model ------------------------------------------------
 
-# JC: Lotka-Volterra models aren't running...
-# initializing in regions that generate negative expected values for negBinom
-# I've solved this in the past with binding lambda to be > 25
+RickUB <- stan(file = file.path("Stan_Mods", "fit_RickUB.stan"), 
+             data = stan_dat, chains = 4, cores = detectCores())
 
-BevB <- stan(file = file.path("Stan_Mods", "BevB.stan"), 
-             data = stan_dat, chains = 2, cores = detectCores())
+saveRDS(RickUB, file = file.path("RDS_Files", "fit_RickUB.RDS"))
 
-RickB <- stan(file = file.path("Stan_Mods", "RickB.stan"), 
-             data = stan_dat, chains = 2, cores = detectCores())
+GermMod <- stan(file = file.path("Stan_Mods", "germ.stan"), 
+                data = stan_dat2, chains = 4, cores = detectCores())
+ 
+saveRDS(GermMod, file = file.path("RDS_Files", "germ.RDS"))    
 
-RickUB <- stan(file = file.path("Stan_Mods", "RickUB.stan"), 
-             data = stan_dat, chains = 2, cores = detectCores())
+MortMod <- stan(file = file.path("Stan_Mods", "mort.stan"), 
+                data = stan_dat3, chains = 4, cores = detectCores())
 
-# Model comparison --------------------------------------------------------
-
-loo_compare(#loo(LotB), loo(lotUB), 
-            loo(BevB), loo(RickB), loo(RickUB))
+saveRDS(MortMod, file = file.path("RDS_Files", "mort.RDS"))  
